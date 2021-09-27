87a6c43b26ac8f1fdeed05542ddb2382393654cd
skdus <skdus@LAPTOP-RRGAGL67>
2021-09-27 12:36:42 +0900

    while

--- Teat/src/While/While5.java
+++ Teat/src/While/While5.java
@@ -1,5 +1,5 @@
 package While;
-
+//시작과 끝이 명확한 횟수를 알 수 있을때는 while문 보다는 for문 선호
 public class While5 {
 
 	public static void main(String[] args) {
@@ -11,7 +11,6 @@ public class While5 {
 			while (j <= 9) { // for(int j=2; j<=9; j++){}
 				System.out.printf("%d * %d = %d \t", j, i, (i * j)); // digit
 				j++; //증감 처리
-
 			}
 			j = 2; // j값 재 초기화, vs for문은 블럭을 시작할때 마다 초기화 되지만, while문은 초기화를 별도로 해주어야함.
 			i++; //증감 처리
