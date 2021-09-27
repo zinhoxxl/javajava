ee8d6bd1022b1ec7ad2fa88500b2611bcf85286f
skdus <skdus@LAPTOP-RRGAGL67>
2021-09-27 12:36:58 +0900

    DoWhile

---
+++ Teat/src/DoWhile/DoWhile1.java
@@ -0,0 +1,16 @@
+package DoWhile;
+
+public class DoWhile1 {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		do {
+			System.out.println("¾È³ç");
+			i++;
+
+		} while (i < 10);
+
+	}
+
+}
---
+++ Teat/src/DoWhile/DoWhileSample.java
@@ -0,0 +1,17 @@
+package DoWhile;
+
+public class DoWhileSample {
+
+	public static void main(String[] args) {
+
+		char c = 'a';
+
+		do {
+			System.out.println(c);
+			c = (char) (c + 1);
+			System.out.println(c);
+		} while (c <= 'z');
+
+	}
+
+}
