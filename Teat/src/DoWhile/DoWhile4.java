253b5a009319a158379194074c1807efe41ddae1
skdus <skdus@LAPTOP-RRGAGL67>
2021-09-27 12:50:15 +0900

    DoWhile

---
+++ Teat/src/DoWhile/DoWhile4.java
@@ -0,0 +1,21 @@
+package DoWhile;
+
+public class DoWhile4 {
+
+	public static void main(String[] args) {
+
+		int i = 1, j = 2;
+		// 중첩 do~while문으로 구구단 출력
+		do {
+			do {
+				System.out.print(j + "*" + i + "=" + (i * j) + "\t");
+				j++;
+			} while (j <= 9);
+			i++;
+			j = 2;
+			System.out.println();
+		} while (i <= 9);
+
+	}
+
+}
