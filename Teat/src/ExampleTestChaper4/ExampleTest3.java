0b3a8c23cff56cc8fff760180ccf378d8a804062
skdus <skdus@LAPTOP-RRGAGL67>
2021-09-27 22:49:01 +0900

    chapter4exam

---
+++ Teat/src/ExampleTestChaper4/ExampleTest3.java
@@ -0,0 +1,17 @@
+package ExampleTestChaper4;
+
+public class ExampleTest3 {
+
+	public static void main(String[] args) {
+
+		for (int x = 1; x <= 10; x++) {
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + "," + y + ")");
+				}
+			}
+		}
+
+	}
+
+}
---
+++ Teat/src/ExampleTestChaper4/ExampleTest4.java
@@ -0,0 +1,18 @@
+package ExampleTestChaper4;
+
+public class ExampleTest4 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+				if (j == i) {
+					System.out.println();
+				}
+			}
+		}
+
+	}
+
+}
