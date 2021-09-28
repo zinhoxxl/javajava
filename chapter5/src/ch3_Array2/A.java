3f06a3a0d066c9e78762725ba3fe8448f1997517
skdus <skdus@LAPTOP-RRGAGL67>
2021-09-28 12:49:32 +0900

    chapter5 Array

---
+++ chapter5/src/ch3_Array2/A.java
@@ -0,0 +1,23 @@
+package ch3_Array2;
+
+public class A {
+
+	// 타입[] 변수명;
+	// String[] args
+	// main()메소드의
+	// 매개변수부분()에 들어가는 배열String[] args
+	// 는 길이가 정해져 있지않은 배열
+	// argument의 갯수만큼 String[] 배열로 생성되어서 전달.
+	// java A hello world my name is jinho
+	// => String[] args= {hello, world, my, name, is, jinho};
+	public static void main(String[] args) {
+		
+		System.out.println("배열의 길이:" + args.length);
+		System.out.println("배열의 첫번째 값:" + args[0]);
+		System.out.println("배열의 두번째 값:" + args[1]);
+
+		for (int i = 0; i < args.length; i++)
+			System.out.println(args[i]);
+	}
+
+}
