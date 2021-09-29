df9178c49a1f6c8a46308556f28e9a2632d5e7e6
skdus <skdus@LAPTOP-RRGAGL67>
2021-09-29 11:51:33 +0900

    chapter5 Array copy

--- chapter5/src/ch3_Array/Ex12_CopyExam.java
+++ chapter5/src/ch3_Array/Ex12_CopyExam.java
@@ -14,8 +14,12 @@ public class Ex12_CopyExam {
 		for(int i =0; i<arr2.length;i++) System.out.print(arr2[1]);
 		System.out.println();
 		System.out.println("----------------");
-		System.out.println("arr1의 hash코드값:"+arr1.hashCode());
+		System.out.println("arr1의 hash코드값:"+arr1.hashCode()); // 객체가 저장된 주소값을 해시방정식으로 출력한 값
 		System.out.println("arr2의 hash코드값:"+arr2.hashCode());
+		
+		System.out.println("arr1의 hash코드값:"+System.identityHashCode(arr1));
+		System.out.println("arr2의 hash코드값:"+System.identityHashCode(arr2));
+		
 		System.out.println("----------------");
 
 		// 2.깊은 복사 - 배열 객체 2개 생성
@@ -24,14 +28,17 @@ public class Ex12_CopyExam {
 		for(int i=0;i<arr3.length;i++) // 요소들을 넘김
 			arr4[i] = arr3[3];
 		
-		for(int i =0; i<arr3.length;i++) System.out.print(arr3[1]);
+		for(int i =0; i<arr3.length;i++) System.out.print(arr3[1]); 
 		System.out.println();
 		for(int i =0; i<arr4.length;i++) System.out.print(arr4[1]);
 		System.out.println();
 		System.out.println("----------------");
 		System.out.println("arr3의 hash코드값:"+arr3.hashCode());
 		System.out.println("arr4의 hash코드값:"+arr4.hashCode());
-
+		
+		System.out.println("arr1의 hash코드값:"+System.identityHashCode(arr3));
+		System.out.println("arr2의 hash코드값:"+System.identityHashCode(arr4));
+ 
 	}
 
 }
