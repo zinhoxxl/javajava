d443a3e40b00026cc3580351275905fbb5ae69b9
skdus <skdus@LAPTOP-RRGAGL67>
2021-10-12 15:49:09 +0900

    chapter7 Interface

--- chapter7/src/ch05/InterfaceC.java
+++
@@ -1,9 +0,0 @@
-package ch05;
-
-public interface InterfaceC extends InterfaceA, InterfaceB {
-	void methodC();
-
-}
-class CA implements C {
-
-}
---
+++ chapter7/src/ch11/LendableEx.java
@@ -0,0 +1,34 @@
+package ch11;
+
+public class LendableEx {
+	public static void main(String[] args) {
+
+		SeperateVolume sv = new SeperateVolume("1111","대지","펄벅");
+		SeperateVolume sv2 = new SeperateVolume("2222","쉬운 JSP","홍길동");
+		
+		// 대여
+		printState(sv);
+		sv.checkOut("수지","2021/03/25");
+		printState(sv);
+		// 반납
+		sv.checkIn();
+		
+		
+		printState(sv2);
+		sv.checkOut("꺽정","2021/08/15");
+		printState(sv2);
+		sv.checkIn();
+		
+	}
+
+	static void printState(SeperateVolume sv) {
+		if(sv.state == sv.BORROWED) {
+			System.out.println("==========대출중인 책===========");
+			System.out.println("대출자:"+ sv.name);
+			System.out.println("대출일:"+ sv.date);
+		}else {
+			System.out.println("==========대출 가능 책===========");
+		}
+	}
+	
+}
