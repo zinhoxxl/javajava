f77f3db8efcb3c9b5a9a6bb86d2fc46363c4d3bd
skdus <skdus@LAPTOP-RRGAGL67>
2021-10-12 09:45:46 +0900

    chapter6!!

--- chapter6_02/src/ExamChapter6_02_07/SnowTireExample.java
+++ chapter6_02/src/ExamChapter6_02_07/SnowTireExample.java
@@ -6,10 +6,23 @@ public class SnowTireExample {
 
 		SnowTire snowTire = new SnowTire();
 		Tire tire = snowTire;
-		
+
 		snowTire.run(); // 자식으로 호출
-		tire.run(); // 부모로 호출     
-		
+		tire.run(); // 부모로 호출
+	}
+}
+
+class Tire { // 부모
+
+	public void run() {
+		System.out.println("일반타이어가 굴러갑니다");
+	}
+}
+
+class SnowTire extends Tire {
+	@Override
+	public void run() { // run을 재정의
+		System.out.println("스노우 타이어가 굴러갑니다");
 	}
 
-}  
+}
--- chapter6_02/src/ExamChapter6_02_07/Tire.java
+++
@@ -1,9 +0,0 @@
-package ExamChapter6_02_07;
-
-public class Tire { // 부모
-
-     public void run() {
-    	 System.out.println("일반타이어가 굴러갑니다");
-     }
-	
-}
